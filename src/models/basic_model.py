a73e7bb37792cce4d4bdeaee5adf4a963662a99a
lrtanner <lrtanner@ucsc.edu>
2025-02-17 18:21:48 -0800

    Implemented basic_model
    Total params of basic_model is 148,467

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -1,15 +1,30 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.optimizers import RMSprop, Adam
+from tensorflow.keras.layers import Rescaling
+from tensorflow.keras.optimizers import Adam
 
 class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
-        # Your code goes here
-        # you have to initialize self.model to a keras model
-        pass
+        self.model = Sequential([
+            Rescaling(1./255, input_shape=input_shape),  # Normalize pixel values to [0,1]
+            
+            layers.Conv2D(32, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D(pool_size=(2,2)),
+
+            layers.Conv2D(32, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D(pool_size=(2,2)),
+
+            layers.Conv2D(48, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D(pool_size=(2,2)),
+
+            layers.Flatten(),
+            layers.Dense(8, activation='relu'),
+            layers.Dense(categories_count, activation='softmax')  # Output layer
+        ])
     
     def _compile_model(self):
-        # Your code goes here
-        # you have to compile the keras model, similar to the example in the writeup
-        pass
+        self.model.compile(
+            optimizer=Adam(learning_rate=0.001),
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy']
+        )
