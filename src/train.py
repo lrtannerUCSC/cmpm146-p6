0d30789c3c202a7617573dfd16e8226946d737af
WontonsofDMG <mitchelltai221@gmail.com>
2025-02-18 10:47:04 -0800

    Implemented model summary printer into folder summary

--- src/train.py
+++ src/train.py
@@ -5,6 +5,8 @@ from models.model import Model
 from config import image_size
 import matplotlib.pyplot as plt
 import time
+import os
+import sys
 
 input_shape = (image_size[0], image_size[1], 3)
 categories_count = 3
@@ -36,6 +38,15 @@ def plot_history(history):
     plt.legend()
     plt.xlabel('Epoch')
     plt.show()
+    
+def get_unique_filename(directory, base_filename):
+    filename = base_filename
+    counter = 1
+    while os.path.exists(os.path.join(directory, filename)):
+        filename = f"{base_filename}_{counter}.txt"
+        counter += 1
+    return filename
+
 
 if __name__ == "__main__":
     # if you want to load your model later, you can use:
@@ -52,7 +63,23 @@ if __name__ == "__main__":
     model_class = models[name]
     print('* Training {} for {} epochs'.format(name, epochs))
     model = model_class(input_shape, categories_count)
+    
     model.print_summary()
+
+    # Create summary directory if it doesn't exist
+    summary_dir = os.path.join(os.path.dirname(__file__), 'summary')
+    os.makedirs(summary_dir, exist_ok=True)
+    
+    # Get unique filename
+    base_filename = 'model_summary'
+    summary_file = get_unique_filename(summary_dir, base_filename)
+    
+    # Redirect stdout to a file for model summary
+    with open(os.path.join(summary_dir, summary_file), 'w') as f:
+        sys.stdout = f
+        model.print_summary()
+        sys.stdout = sys.__stdout__
+    
     history = model.train_model(train_dataset, validation_dataset, epochs)
     print('* Evaluating {}'.format(name))
     model.evaluate(test_dataset)
