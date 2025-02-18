5d2964363bf4f42307050aae7b54845e5ec12108
lrtanner <lrtanner@ucsc.edu>
2025-02-17 23:07:27 -0800

    basic_model has 73% accuracy after 10 epochs

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -8,20 +8,20 @@ class BasicModel(Model):
         self.model = Sequential([
             Rescaling(1./255, input_shape=input_shape),  # Normalize input pixels
 
+            layers.Conv2D(8, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D(pool_size=(2,2)),
+
+            layers.Conv2D(16, (3,3), activation='relu', padding='same'),
+            layers.MaxPooling2D(pool_size=(2,2)),
+
             layers.Conv2D(32, (3,3), activation='relu', padding='same'),
             layers.MaxPooling2D(pool_size=(2,2)),
 
             layers.Conv2D(64, (3,3), activation='relu', padding='same'),
             layers.MaxPooling2D(pool_size=(2,2)),
 
-            layers.Conv2D(128, (3,3), activation='relu', padding='same'),
-            layers.MaxPooling2D(pool_size=(2,2)),
-
-            layers.Conv2D(256, (3,3), activation='relu', padding='same'),
-            layers.MaxPooling2D(pool_size=(2,2)),
-
             Flatten(),
-            layers.Dense(128, activation='relu'),  # Fully connected layer with reasonable params
+            layers.Dense(16, activation='relu'),  # Fully connected layer with reasonable params
             layers.Dense(categories_count, activation='softmax')  # Output layer
         ])
     
